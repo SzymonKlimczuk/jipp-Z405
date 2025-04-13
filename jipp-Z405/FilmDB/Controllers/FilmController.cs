d9aee203686cf3bfe6ae178ff034ab7f914e9621
Szymon Klimczuk <Szymonklimczuk1234@gmail.com>
2025-04-13 12:42:13 +0200

    init

--- jipp-Z405/FilmDB/Controllers/FilmController.cs
+++ jipp-Z405/FilmDB/Controllers/FilmController.cs
@@ -1,26 +1,59 @@
 ﻿using FilmDB.Data;
 using FilmDB.Models;
+using FilmDB.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmDB.Controllers
 {
     public class FilmController : Controller
     {
-        private FilmContext _context;
-        public FilmController( FilmContext context )
+        private FilmManager _manager;
+        public FilmController( FilmManager manager )
         {
-            _context = context;
+            _manager = manager;
         }
         public IActionResult Index()
         {
-            var film = new FilmModel();
-            film.Id = 0;
-            film.Title = "Tenet";
-            film.Year = "2020";
-            _context.Films.Add(film);
-            _context.SaveChanges();
-
+            var films = _manager;
             return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(FilmModel film)
+        {
+            _manager.AddFilm(film);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult delete(int id)
+        {
+            var filmToRemove = _manager.GetFilm(id);
+            return View(filmToRemove);
+        }
+        [HttpPost]
+        public IActionResult Remove(int id)
+        { 
+            _manager.RemoveFilm(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var filmToEdit = _manager.GetFilm(id);
+            return View(filmToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(FilmModel film)
+        {
+            _manager.UpdateFilm(film);
+            return RedirectToAction("Index");
         }
     }
 }
