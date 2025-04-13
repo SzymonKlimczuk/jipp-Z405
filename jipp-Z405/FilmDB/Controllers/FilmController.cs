d510035e7e1529aa1302a9866b1834aaf36ab81b
Szymon Klimczuk <Szymonklimczuk1234@gmail.com>
2025-04-13 02:00:56 +0200

    init

---
+++ jipp-Z405/FilmDB/Controllers/FilmController.cs
@@ -0,0 +1,26 @@
+﻿using FilmDB.Data;
+using FilmDB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDB.Controllers
+{
+    public class FilmController : Controller
+    {
+        private FilmContext _context;
+        public FilmController( FilmContext context )
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var film = new FilmModel();
+            film.Id = 0;
+            film.Title = "Tenet";
+            film.Year = "2020";
+            _context.Films.Add(film);
+            _context.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ jipp-Z405/FilmDB/Models/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmDB.Models
+{
+    public class FilmModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+    }
+}
